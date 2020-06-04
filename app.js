e452356b1f8b634958e5a6c928fd07a2a5061d05
Paul Aglipay <paglipay@gmail.com>
2020-06-04 12:09:21 -0700

    Updated Classes

--- app.js
+++ app.js
@@ -2,34 +2,71 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
+const path = require('path');
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const prompts = async function () {
+    // Write code to use inquirer to gather information about the development team members,
+    // and to create objects for each team member (using the correct classes as blueprints!)
+    const employees = [];
+
+    const answers = await inquirer
+        .prompt([
+            {
+                message: "name:",
+                name: "name"
+            },
+            {
+                message: "id:",
+                name: "id"
+            },
+            {
+                message: "email:",
+                name: "email"
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Which is better?',
+                choices: ['Manager', 'Engineer', 'Intern'],
+            },
+        ]);
+
+    //console.info('Answer:', answers.role);
+    if (answers.role === 'Manager') {
+        const last_answers = await inquirer
+        .prompt([
+            {
+                message: "Office numbef:",
+                name: "off_num"
+            }
+        ]);
+        const empObj = new Manager(answers.name, answers.id, answers.email, last_answers.off_num);
+        employees.push(empObj);
+    }
+    else if (answers.role === 'Engineer') {
+        const empObj = new Engineer(answers.name, answers.id, answers.email, "1231231234");
+        employees.push(empObj);
+    }
+    else if (answers.role === 'Intern') {
+        const empObj = new Intern(answers.name, answers.id, answers.email, "1231231234");
+        employees.push(empObj);
+    }
+
+
+    fs.writeFile(outputPath, render(employees), function (err) {
+        console.log('wrote')
+    });
+}
+
+
+const init = async function () {
+    prompts();
+}
+
+init();
