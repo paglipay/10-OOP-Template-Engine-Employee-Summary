5d1a990f833090211ad256d8cfd8a76b9ff92ac1
Paul Aglipay <paglipay@gmail.com>
2020-06-04 12:42:59 -0700

    Updated to pass tests

--- app.js
+++ app.js
@@ -9,64 +9,77 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const employees = [];
 const prompts = async function () {
     // Write code to use inquirer to gather information about the development team members,
     // and to create objects for each team member (using the correct classes as blueprints!)
-    const employees = [];
+    let employee_ask = true;
+    while (employee_ask) {
+        const answers = await inquirer
+            .prompt([
+                {
+                    message: "name:",
+                    name: "name"
+                },
+                {
+                    message: "id:",
+                    name: "id"
+                },
+                {
+                    message: "email:",
+                    name: "email"
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'Which is better?',
+                    choices: ['Manager', 'Engineer', 'Intern'],
+                },
+            ]);
 
-    const answers = await inquirer
-        .prompt([
-            {
-                message: "name:",
-                name: "name"
-            },
-            {
-                message: "id:",
-                name: "id"
-            },
-            {
-                message: "email:",
-                name: "email"
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: 'Which is better?',
-                choices: ['Manager', 'Engineer', 'Intern'],
-            },
-        ]);
+        //console.info('Answer:', answers.role);
+        if (answers.role === 'Manager') {
+            const last_answers = await inquirer
+                .prompt([
+                    {
+                        message: "Office numbef:",
+                        name: "off_num"
+                    }
+                ]);
+            const empObj = new Manager(answers.name, answers.id, answers.email, last_answers.off_num);
+            employees.push(empObj);
+        }
+        else if (answers.role === 'Engineer') {
+            const empObj = new Engineer(answers.name, answers.id, answers.email, "1231231234");
+            employees.push(empObj);
+        }
+        else if (answers.role === 'Intern') {
+            const empObj = new Intern(answers.name, answers.id, answers.email, "1231231234");
+            employees.push(empObj);
+        }
 
-    //console.info('Answer:', answers.role);
-    if (answers.role === 'Manager') {
-        const last_answers = await inquirer
-        .prompt([
-            {
-                message: "Office numbef:",
-                name: "off_num"
-            }
-        ]);
-        const empObj = new Manager(answers.name, answers.id, answers.email, last_answers.off_num);
-        employees.push(empObj);
+        const { keep_going } = await inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'keep_going',
+                    message: 'Add another?',
+                    choices: ['Yes', 'No'],
+                }
+            ]);
+
+        if (keep_going === 'No') {
+            employee_ask = false;
+        }
     }
-    else if (answers.role === 'Engineer') {
-        const empObj = new Engineer(answers.name, answers.id, answers.email, "1231231234");
-        employees.push(empObj);
-    }
-    else if (answers.role === 'Intern') {
-        const empObj = new Intern(answers.name, answers.id, answers.email, "1231231234");
-        employees.push(empObj);
-    }
-
-
-    fs.writeFile(outputPath, render(employees), function (err) {
-        console.log('wrote')
-    });
 }
 
 
 const init = async function () {
-    prompts();
+    await prompts();
+    fs.writeFile(outputPath, render(employees), function (err) {
+        console.log('wrote')
+    });
 }
 
 init();
